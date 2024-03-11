08e51fd9184e07de52b7e73538c156bff1e9583a
Julien RICHARD <julien.y.richard@gmail.com>
2024-03-11 14:16:03 +0100

    correction de texte

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="container m-4">
       <h1 className="text-center mb-5">
-        Hello dans votre application Taste and share
+        Hello dans votre application Taste and share.
       </h1>
       <h3 className="text-center m-3">
         Commencez dès maintenant en vous
@@ -14,7 +14,7 @@ function App() {
       </h3>
       <h3 className="text-center m-3">
         Ou <Link to="/connexion">connectez</Link> vous si vous avez déjà un
-        compte
+        compte.
       </h3>
     </div>
   );
